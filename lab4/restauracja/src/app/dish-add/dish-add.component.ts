65ee70410a4d212bf02235b7e7596f91df6fd3af
Mikołaj Wielgos <43356643+wlgs@users.noreply.github.com>
2021-12-18 15:43:19 +0100

    small changes

--- lab4/restauracja/src/app/dish-add/dish-add.component.ts
+++ lab4/restauracja/src/app/dish-add/dish-add.component.ts
@@ -8,67 +8,91 @@ import { Validators } from '@angular/forms';
   styleUrls: ['./dish-add.component.css'],
 })
 export class DishAddComponent implements OnInit {
-  constructor() {}
+  constructor() { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 
   @Output() formSubmitEvent = new EventEmitter<Dish>();
 
   dishAddForm = new FormGroup({
-    dishname: new FormControl('', [Validators.required, Validators.minLength(2)]),
-    dishtype: new FormControl('', [Validators.required, Validators.minLength(2)]),
-    dishcategory: new FormControl('', [Validators.required, Validators.minLength(2)]),
-    dishingredients: new FormControl('', [Validators.required, Validators.minLength(2)]),
-    dishmaxperday: new FormControl('', [Validators.required, Validators.min(1), Validators.pattern("[0-9]*")]),
-    dishprice: new FormControl('', [Validators.required, Validators.min(1), Validators.pattern("[0-9]*\.?[0-9]+")]),
-    dishshortdesc: new FormControl('', [Validators.required, Validators.minLength(2)]),
-    dishimagelink: new FormControl('', [Validators.required, Validators.minLength(2)]),
+    dishname: new FormControl('', [
+      Validators.required,
+      Validators.minLength(2),
+    ]),
+    dishtype: new FormControl('', [
+      Validators.required,
+      Validators.minLength(2),
+    ]),
+    dishcategory: new FormControl('', [
+      Validators.required,
+      Validators.minLength(2),
+    ]),
+    dishingredients: new FormControl('', [
+      Validators.required,
+      Validators.minLength(2),
+    ]),
+    dishmaxperday: new FormControl('', [
+      Validators.required,
+      Validators.min(1),
+      Validators.pattern('[0-9]*'),
+    ]),
+    dishprice: new FormControl('', [
+      Validators.required,
+      Validators.min(1),
+      Validators.pattern('[0-9]*.?[0-9]+'),
+    ]),
+    dishshortdesc: new FormControl('', [
+      Validators.required,
+      Validators.minLength(2),
+    ]),
+    dishimagelink: new FormControl('', [
+      Validators.required,
+      Validators.minLength(2),
+    ]),
   });
 
+  showError = false;
+  showOk = false;
 
-  showError = false
-  showOk = false
-
-  submitForm(){
-    console.log(this.dishAddForm)
-    console.log(this.dishAddForm.valid)
-    if (!this.dishAddForm.valid){
-      this.showError = true
-      return
+  submitForm() {
+    console.log(this.dishAddForm);
+    console.log(this.dishAddForm.valid);
+    if (!this.dishAddForm.valid) {
+      this.showError = true;
+      return;
     }
     let newDish = {
-      name:this.dishAddForm.get("dishname")!.value,
-      type:this.dishAddForm.get("dishtype")!.value,
-      category:this.dishAddForm.get("dishcategory")!.value,
-      ingredients:this.dishAddForm.get("dishingredients")!.value,
-      maxperday:this.dishAddForm.get("dishmaxperday")!.value,
-      price:this.dishAddForm.get("dishprice")!.value,
-      shortdesc:this.dishAddForm.get("dishshortdesc")!.value,
-      imagelink:this.dishAddForm.get("dishimagelink")!.value,
-      amount:0,
-      currency:"$",
-      likes:0,
-      dislikes:0
-    } as Dish
-    this.formSubmitEvent.emit(newDish)
-    this.showError = false
-    this.showOk = true
-    this.dishAddForm.reset()
+      name: this.dishAddForm.get('dishname')!.value,
+      type: this.dishAddForm.get('dishtype')!.value,
+      category: this.dishAddForm.get('dishcategory')!.value,
+      ingredients: this.dishAddForm.get('dishingredients')!.value,
+      maxperday: this.dishAddForm.get('dishmaxperday')!.value,
+      price: this.dishAddForm.get('dishprice')!.value,
+      shortdesc: this.dishAddForm.get('dishshortdesc')!.value,
+      imagelink: this.dishAddForm.get('dishimagelink')!.value,
+      amount: 0,
+      currency: '$',
+      likes: 0,
+      dislikes: 0,
+    } as Dish;
+    this.formSubmitEvent.emit(newDish);
+    this.showError = false;
+    this.showOk = true;
+    this.dishAddForm.reset();
   }
-
 }
 
-export interface Dish{
-  name:string;
-  type:string;
-  category:string;
-  ingredients:string;
-  maxperday:number;
-  price:number;
-  shortdesc:string;
-  imagelink:string;
-  amount:number;
-  currency:string;
-  likes:number;
-  dislikes:number;
+export interface Dish {
+  name: string;
+  type: string;
+  category: string;
+  ingredients: string;
+  maxperday: number;
+  price: number;
+  shortdesc: string;
+  imagelink: string;
+  amount: number;
+  currency: string;
+  likes: number;
+  dislikes: number;
 }
--- lab4/restauracja/src/app/dishes/dishes.component.ts
+++ lab4/restauracja/src/app/dishes/dishes.component.ts
@@ -11,53 +11,53 @@ export class DishesComponent implements OnInit {
 
   dishes: Dish[] = []
 
-  
+
   ngOnInit(): void {
     fetch('./assets/data/dishes.json').then(res => res.json())
-    .then(json => {
-      for (let p in json["Dishes"]){
-        this.dishes.push({
-          name: json["Dishes"][p]["Name"],
-          type: json["Dishes"][p]["Type"],
-          category: json["Dishes"][p]["Category"],
-          ingredients: json["Dishes"][p]["Ingredients"],
-          maxperday: json["Dishes"][p]["MaxPerDay"],
-          price: json["Dishes"][p]["Price"],
-          shortdesc: json["Dishes"][p]["ShortDesc"],
-          imagelink: json["Dishes"][p]["ImageLink"],
-          amount:0,
-          currency:json["Dishes"][p]["Currency"],
-          likes:json["Dishes"][p]["Likes"],
-          dislikes:json["Dishes"][p]["Dislikes"]
-        } as Dish)
-      }
-    });
+      .then(json => {
+        for (let p in json["Dishes"]) {
+          this.dishes.push({
+            name: json["Dishes"][p]["Name"],
+            type: json["Dishes"][p]["Type"],
+            category: json["Dishes"][p]["Category"],
+            ingredients: json["Dishes"][p]["Ingredients"],
+            maxperday: json["Dishes"][p]["MaxPerDay"],
+            price: json["Dishes"][p]["Price"],
+            shortdesc: json["Dishes"][p]["ShortDesc"],
+            imagelink: json["Dishes"][p]["ImageLink"],
+            amount: 0,
+            currency: json["Dishes"][p]["Currency"],
+            likes: json["Dishes"][p]["Likes"],
+            dislikes: json["Dishes"][p]["Dislikes"]
+          } as Dish)
+        }
+      });
   }
 
-  addClick(dish: Dish){
-    if(dish.amount<dish.maxperday)
+  addClick(dish: Dish) {
+    if (dish.amount < dish.maxperday)
       dish.amount += 1
   }
 
-  removeClick(dish: Dish){
+  removeClick(dish: Dish) {
     console.log(dish)
-    if(dish.amount>=1)
+    if (dish.amount >= 1)
       dish.amount -= 1
   }
 
-  getOrderedAmount(dishes: Dish[]): number{
+  getOrderedAmount(dishes: Dish[]): number {
     let amount = 0;
-    for (let dish of dishes){
+    for (let dish of dishes) {
       amount += dish.amount
     }
     return amount;
   }
 
-  getMaxPriceDish(dishes: Dish[]): Dish{
+  getMaxPriceDish(dishes: Dish[]): Dish {
     let max = -1
     let maxDish = <Dish>{}
-    for (let dish of dishes){
-      if (dish.price > max){
+    for (let dish of dishes) {
+      if (dish.price > max) {
         max = dish.price
         maxDish = dish
       }
@@ -65,11 +65,11 @@ export class DishesComponent implements OnInit {
     return maxDish
   }
 
-  getMinPriceDish(dishes: Dish[]): Dish{
+  getMinPriceDish(dishes: Dish[]): Dish {
     let min = 9999
     let minDish = <Dish>{}
-    for (let dish of dishes){
-      if (dish.price < min){
+    for (let dish of dishes) {
+      if (dish.price < min) {
         min = dish.price
         minDish = dish
       }
@@ -77,25 +77,25 @@ export class DishesComponent implements OnInit {
     return minDish
   }
 
-  deleteDish(dish: Dish){
+  deleteDish(dish: Dish) {
     for (var _i = 0; _i < this.dishes.length; _i++) {
-      if (this.dishes[_i] == dish){
-        this.dishes.splice(_i,1)
+      if (this.dishes[_i] == dish) {
+        this.dishes.splice(_i, 1)
         return
       }
     }
   }
 
 
-  formSubmitEventHandler(dish: Dish){
+  formSubmitEventHandler(dish: Dish) {
     this.dishes.push(dish)
   }
 
-  ratingEventHandler(dish: Dish, ev: any){
-    if (ev == 1){
+  ratingEventHandler(dish: Dish, ev: any) {
+    if (ev == 1) {
       dish.likes += 1
     }
-    else{
+    else {
       dish.dislikes += 1
     }
   }
@@ -105,17 +105,17 @@ export class DishesComponent implements OnInit {
 
 
 
-export interface Dish{
-  name:string;
-  type:string;
-  category:string;
-  ingredients:string;
-  maxperday:number;
-  price:number;
-  shortdesc:string;
-  imagelink:string;
-  amount:number;
-  currency:string;
-  likes:number;
-  dislikes:number;
+export interface Dish {
+  name: string;
+  type: string;
+  category: string;
+  ingredients: string;
+  maxperday: number;
+  price: number;
+  shortdesc: string;
+  imagelink: string;
+  amount: number;
+  currency: string;
+  likes: number;
+  dislikes: number;
 }
